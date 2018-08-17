fc5da8340c36510af064353419ddbfe2edf5ec9e
mybalde <34603484+mybalde@users.noreply.github.com>
2018-08-17 16:21:46 +0200

    Add files via upload

---
+++ memoire/src/lamalloc/leslie.h
@@ -0,0 +1,10 @@
+#ifndef _LESLIE_H
+#define _LESLIE_H
+
+#include <stdio.h>
+
+void cfree (char *ap);
+char *cmalloc (int nbytes);
+void i_alloc (void);
+
+#endif
---
+++ memoire/src/mbmalloc/freelist_malloc/malloc.h
@@ -0,0 +1,7 @@
+#ifndef MALLOC_H_INCLUDED
+#define MALLOC_H_INCLUDED
+
+void *cmalloc (size_t size);
+void cfree (void *p);
+void cscan ();
+#endif // MALLOC_H_INCLUDED
